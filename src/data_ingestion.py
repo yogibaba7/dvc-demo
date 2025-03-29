6cc48c77cef40340cb2d34b6821d6eef45a41257
yogibaba7 <yogeshchouhan263@gmail.com>
2025-03-30 00:38:41 +0530

    second experiment

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 df = pd.read_csv('https://raw.githubusercontent.com/yogibaba7/EndToEndMlproject/refs/heads/main/artifacts/train.csv')
 
-df = df[['gender','lunch','math_score','reading_score','writing_score']]
+df = df[['gender','math_score','reading_score','writing_score']]
 
-df = df[df['math_score']>70]
+df = df[df['math_score']>80]
 
 df.to_csv(os.path.join('data','students.csv'))
